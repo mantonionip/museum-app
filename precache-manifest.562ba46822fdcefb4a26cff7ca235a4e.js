a128cfd264e5a4eba4e9daf798c213a754aa265f
Mehdi Pilehvarian <mehdi.pilehvarian@gmail.com>
2019-04-27 14:39:32 -0400

    Updates

---
+++ precache-manifest.562ba46822fdcefb4a26cff7ca235a4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de722bfdc9bf859437d620f9ab44c94",
+    "url": "/museum-app/index.html"
+  },
+  {
+    "revision": "28d43c4d73a3e33d6f94",
+    "url": "/museum-app/static/css/main.b8301a8a.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/museum-app/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "28d43c4d73a3e33d6f94",
+    "url": "/museum-app/static/js/main.283f89f2.chunk.js"
+  },
+  {
+    "revision": "c2f95ec4949fdd8260ca",
+    "url": "/museum-app/static/js/runtime~main.4a9d3690.js"
+  }
+]);
